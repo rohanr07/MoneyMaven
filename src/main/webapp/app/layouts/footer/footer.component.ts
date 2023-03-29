caf3aae09ab8a6b18a101ad0e1d31d55df6c043c
Anurag Tiwari <apt123@student.bham.ac.uk>
2023-03-29 10:21:41 +0100

    edited footer and navbar layouts

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor(private router: Router) {}
+  loadGDPR() {
+    this.router.navigate(['/gdpr']);
+  }
+  shouldShowFooter() {
+    return this.router.url != '/gdpr';
+  }
+}
