be9922786ffc06e815613a6699f66482a4193621
Prabal Pratap Singh <pxp131@student.bham.ac.uk>
2023-05-01 02:47:58 +0530

    changing landing page,login page and some optimization

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -5,13 +5,22 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import dayjs from 'dayjs/esm';
 
 import { AccountService } from 'app/core/auth/account.service';
+import { main } from '@popperjs/core';
+import { Account } from '../../core/auth/account.model';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
 })
 export class MainComponent implements OnInit {
+  account: Account | null = null;
+
   private renderer: Renderer2;
+  public isHomePage: boolean = false;
+
+  private readonly destroy$ = new Subject<void>();
 
   constructor(
     private accountService: AccountService,
@@ -24,6 +33,11 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+
     // try to log in automatically
     this.accountService.identity().subscribe();
 
@@ -31,6 +45,7 @@ export class MainComponent implements OnInit {
       if (event instanceof NavigationEnd) {
         this.updateTitle();
       }
+      this.isHomePage = false;
     });
 
     this.translateService.onLangChange.subscribe((langChangeEvent: LangChangeEvent) => {
@@ -40,7 +55,7 @@ export class MainComponent implements OnInit {
     });
   }
 
-  private getPageTitle(routeSnapshot: ActivatedRouteSnapshot): string {
+  public getPageTitle(routeSnapshot: ActivatedRouteSnapshot): string {
     const title: string = routeSnapshot.data['pageTitle'] ?? '';
     if (routeSnapshot.firstChild) {
       return this.getPageTitle(routeSnapshot.firstChild) || title;
@@ -55,4 +70,13 @@ export class MainComponent implements OnInit {
     }
     this.translateService.get(pageTitle).subscribe(title => this.titleService.setTitle(title));
   }
+  isNotLandingPage(): boolean {
+    return this.router.url !== '/';
+  }
+
+  isNotLoginPage(): boolean {
+    return this.router.url !== '/login';
+  }
+
+  protected readonly main = main;
 }
